103056f631a85426982957cf76c87d8bd6e7ac09
leen21-meet <leen21@meet.mit.edu>
2019-07-28 09:40:06 -0400

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,20 @@
+
+##total = 0
+##for number in range(1, 10 + 1):
+##    print(number)
+##    total = total + number
+##print(total)
+##number= ('bassel')
+##def add_numbers ():
+##    print(number)
+##    total = total + number
+##    return (5)
+##answer = add_numbers 
+##print (answer)
+
+
+
+def example (x):
+    x[0] = "a"
+    return x
+print (example ([1,2,3]))
